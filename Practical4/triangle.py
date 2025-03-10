b197a02af98c30e7867f7f20e6bea6aae2c8c5dc
666 <s2769551@ed.ac.uk>
2025-03-10 08:27:51 +0100

    2025.03.10 trangle.py commit1

---
+++ Practical4/triangle.py
@@ -0,0 +1,4 @@
+sum = 0                         #initialize every "t" as zero
+for i in range(1, 11):          #give the for cycle, start = 1, end = 11
+    sum+=i                      #add every line number into sum
+    print(sum, end = ' ')       #output every outcome
